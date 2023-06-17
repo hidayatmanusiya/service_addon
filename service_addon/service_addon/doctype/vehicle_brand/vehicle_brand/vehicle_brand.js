53e9ec120dfb0430e9a12f2ab5b94c874934fef4
Hidayat ali <63574705+hidayatmanusiya@users.noreply.github.com>
2023-06-17 14:24:46 +0530

    VB Delete

--- service_addon/service_addon/doctype/vehicle_brand/vehicle_brand/vehicle_brand.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Hidayatali and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Vehicle Brand', {
-	// refresh: function(frm) {
-
-	// }
-});
--- service_addon/service_addon/doctype/vehicle_brand/vehicle_brand/vehicle_brand.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, Hidayatali and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class VehicleBrand(Document):
-	pass
