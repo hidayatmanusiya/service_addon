2c88e44d0737940099e1e99aa8a61f9b552b4c34
Hidayat ali <63574705+hidayatmanusiya@users.noreply.github.com>
2023-06-17 14:20:06 +0530

    Vehicle Brand Doctype

---
+++ service_addon/service_addon/doctype/vehicle_brand/vehicle_brand/vehicle_brand.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Hidayatali and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Vehicle Brand', {
+	// refresh: function(frm) {
+
+	// }
+});
